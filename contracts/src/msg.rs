c621277cd614b81fea6c98014b33daf45f08e073
TomL94 <langer.tom7@gmail.com>
2022-06-08 12:55:32 +0300

    Messages and stuff

--- contracts/src/msg.rs
+++ contracts/src/msg.rs
@@ -1,27 +1,29 @@
+use cosmwasm_std::{HumanAddr, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use crate::state::Beneficiary;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Deserialize, JsonSchema)]
 pub struct InitMsg {
-    pub count: i32,
+    tax_denom: Option<String>,
+    beneficiaries: Vec<Beneficiary>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Deserialize, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Increment {},
-    Reset { count: i32 },
+    Withdraw { amount: Option<Uint128> },
+
+    // Admin commands
+    ChangeAdmin { new_admin: HumanAddr },
+    ChangeBeneficiaries {
+        beneficiaries: Vec<Beneficiary>
+    },
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Deserialize, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
-}
-
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+    GetBeneficiaries {},
+    GetBeneficiaryBalance { address: HumanAddr },
 }
--- contracts/src/state.rs
+++ contracts/src/state.rs
@@ -1,21 +1,33 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, Storage};
+use cosmwasm_std::{CanonicalAddr, HumanAddr, Storage};
 use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    admin: HumanAddr,
 }
 
-pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
+pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, Config> {
     singleton(storage, CONFIG_KEY)
 }
 
-pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
+pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, Config> {
     singleton_read(storage, CONFIG_KEY)
 }
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+struct BeneficiaryWeight {
+    rate: u16,
+    decimal_places_in_rate: u8,
+}
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+pub struct Beneficiary {
+    address: HumanAddr,
+    weight: BeneficiaryWeight,
+}
+
